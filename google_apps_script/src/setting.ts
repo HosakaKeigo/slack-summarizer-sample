3b061ca4ca19c96abc61f8969e96aaa84bf70c85
HosakaKeigo <gomagoma313@gmail.com>
2023-07-22 10:48:58 +0900

    update setting.ts

--- google_apps_script/src/setting.ts
+++ google_apps_script/src/setting.ts
@@ -1,2 +1,28 @@
 const CHUNK_SIZE = 12000; // 16kを想定
 const DEFAULT_MODEL = "gpt-3.5-turbo-16k-0613"
+
+// Google Documentsの共有設定
+const FILE_PERMISSION = GoogleAppsScript.Drive.Access.DOMAIN_WITH_LINK
+const FILE_PERMISSION_TYPE = GoogleAppsScript.Drive.Permission.VIEW
+
+
+// Function calling用関数スキーマ
+const GPT_FUNCTION = [
+  {
+    "name": "createSummarizedDocument",
+    "description": "useful when create a summarized document",
+    "parameters": {
+      "type": "object",
+      "properties": {
+        "title": {
+          "type": "string",
+          "description": "A title for a summary document"
+        },
+        "body": {
+          "type": "string",
+          "description": "A body for a summary document"
+        }
+      },
+      "required": ["title", "summary"]
+    }
+  }]
