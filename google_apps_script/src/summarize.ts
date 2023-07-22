754d63aca80f63df695196f822b44e5e31a9e8e6
HosakaKeigo <gomagoma313@gmail.com>
2023-07-22 10:49:30 +0900

    [refactor] OpenAIの処理を別関数化

--- google_apps_script/src/summarize.ts
+++ google_apps_script/src/summarize.ts
@@ -1,58 +1,64 @@
-interface ChatCompletionResponse {
-  id: string;
-  object: string;
-  created: number;
-  choices: {
-    index: number;
-    message: {
-      role: "assistant";
-      content: string;
-    };
-    finish_reason: string;
-  }[];
-  usage: {
-    prompt_tokens: number;
-    completion_tokens: number;
-    total_tokens: number;
-  };
+/**
+ * GPTでの要約
+ * Function Callingで指定した形式のJSONを返す。
+ */
+function summarize(content: string, model = DEFAULT_MODEL): { title: string, body: string } {
+  const messages: ChatCompletionMessge[] = [
+    { role: "system", content: CHAT_GPT_SYSTEM_PROMPT },
+    { role: "user", content: content },
+  ];
+
+  try {
+    const response = execChatCompletion(messages, model);
+
+    const function_call = response.choices[0].message.function_call
+    if (!function_call) {
+      throw new Error("function_call not found")
+    }
+
+    const resultJSON = JSON.parse(function_call.arguments) as { title: string, body: string }
+    if (!("title" in resultJSON && "body" in resultJSON)) {
+      throw new Error("Function Call: property not found")
+    }
+
+    return resultJSON
+  }
+  catch (e) {
+    if (e instanceof Error) {
+      throw new Error(e.message);
+    }
+  }
 }
 
-function summarize(content: string, model = DEFAULT_MODEL) {
+function execChatCompletion(messages: ChatCompletionMessge[], model: string): ChatCompletionResponse {
   const OPENAI_BASE_URL = "https://api.openai.com/v1";
   const OPENAI_ENDPOINTS = new Map<string, string>([
     ["completion", `${OPENAI_BASE_URL}/chat/completions`],
   ]);
 
-  const apiKey = PropertiesService.getScriptProperties().getProperty("OPENAI_API_KEY");
-
-  const messages = [
-    { role: "system", content: CHAT_GPT_SYSTEM_PROMPT },
-    { role: "user", content: content },
-  ];
-
   const headers = {
-    Authorization: "Bearer " + apiKey,
+    Authorization: "Bearer " + PropertiesService.getScriptProperties().getProperty("OPENAI_API_KEY"),
     "Content-type": "application/json",
   };
-
   const options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
-    muteHttpExceptions: true,
     headers: headers,
     method: "post",
     payload: JSON.stringify({
       model,
-      messages: messages,
+      messages,
+      functions: GPT_FUNCTION,
+      function_call: { name: GPT_FUNCTION[0].name }
     }),
   };
 
   try {
     const response = JSON.parse(
       UrlFetchApp.fetch(OPENAI_ENDPOINTS.get("completion"), options).getContentText()
-    ) as ChatCompletionResponse;
-    console.log(`completion: ${JSON.stringify(response)}`);
-
-    const answer = response.choices[0].message.content;
-    return answer;
+    ) as ChatCompletionResponse | OpenAIError;
+    if ("error" in response) {
+      throw new Error(`${response.error.code}: ${response.error.message}`);
+    }
+    return response
   } catch (e) {
     if (e instanceof Error) {
       throw new Error(e.message);
