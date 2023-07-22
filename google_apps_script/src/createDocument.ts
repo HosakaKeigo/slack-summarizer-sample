5f212c406ae6ad3a70707e4260ec6ca60a2619c1
HosakaKeigo <gomagoma313@gmail.com>
2023-07-22 10:48:42 +0900

    関数名変更

--- google_apps_script/src/createDocument.ts
+++ google_apps_script/src/createDocument.ts
@@ -1,9 +1,9 @@
 /**
  * Googleドキュメント作成。リンクを知っている全員に閲覧権限
  */
-function createDocument(title: string, body: string) {
-  const doc = DocumentApp.create(title).setName(title);
-  doc.getBody().setText(body);
+function createDocument(summary: Summary) {
+  const doc = DocumentApp.create(summary.title);
+  doc.getBody().setText(summary.body);
   const fileId = doc.getId();
   return doc.getId()
 }
