9c59d744b358151a3044c7a5a054d6a8a123ae76
HosakaKeigo <gomagoma313@gmail.com>
2023-07-24 13:52:04 +0900

    slack botへエラー処理追加

--- src/index.ts
+++ src/index.ts
@@ -26,44 +26,40 @@ app.message(async ({ message, client, context, payload }) => {
   }
 
   if (message.subtype === "file_share" && message.files) {
-    const fileInfo = await client.files.info({ file: message.files[0].id })
+    if (message.files.length > 1) {
+      await client.chat.postMessage({
+        text: "ファイルは一つだけ送信してください。",
+        channel: message.channel,
+        thread_ts: payload.event_ts
+      })
+      return
+    }
+
+    const file = message.files[0]
+    if (file.mimetype !== "text/plain") {
+      await client.chat.postMessage({
+        text: ".txtファイルを送信してください。",
+        channel: message.channel,
+        thread_ts: payload.event_ts
+      })
+      return
+    }
+
+    const fileInfo = await client.files.info({ file: file.id })
     if (!fileInfo.content) {
-      await client.chat.postMessage({ text: "すみません、ファイル情報の取得に失敗しました。", channel: message.channel, thread_ts: payload.event_ts })
+      await client.chat.postMessage({
+        text: "すみません、ファイル情報の取得に失敗しました。",
+        channel: message.channel,
+        thread_ts: payload.event_ts
+      })
       return;
     }
+
     postGAS({ content: fileInfo.content, channel: message.channel, thread_ts: payload.event_ts })
     await client.chat.postMessage({ text: `要約中です...完了したらお知らせします。字数：${fileInfo.content.length}`, channel: message.channel, thread_ts: payload.event_ts })
     return
   }
 });
-
-//app.event("file_shared", async ({ payload, client, context }) => {
-//  console.log("file_shared")
-//  // files.infoでURLを取得する
-//  if (context.retryNum) {
-//    console.log(
-//      `skipped retry. retryReason: ${context.retryReason}`
-//    );
-//    return;
-//  }
-
-//  const fileInfo = await client.files.info({ file: payload.file_id })
-//  if (!fileInfo.content) {
-//    await client.chat.postMessage({ text: "すみません、エラーが発生しました。", channel: payload.channel_id, thread_ts: payload.event_ts })
-//    return;
-//  }
-
-//  await client.chat.postMessage({ text: `要約中です...完了したらお知らせします。字数：${fileInfo.content.length}`, channel: payload.channel_id, thread_ts: payload.event_ts })
-
-//  console.log(fileInfo.content)
-//  // テキスト要約
-//  // ３秒以内でレスポンスを返すために別関数に委譲する。
-//  const summary = await summarize(fileInfo.content)
-//  console.log(summary)
-//  // 要約結果を投稿
-//  await client.chat.postMessage({ text: summary + "\n\n" + fileInfo.file?.permalink_public, channel: payload.channel_id, thread_ts: payload.event_ts })
-//  return;
-//});
 
 export const handler = async (event, context, callback) => {
   const handler = await awsLambdaReceiver.start();
