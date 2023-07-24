bb3e7336f616d58cc2e36c92e39a8e91b2de7d9a
HosakaKeigo <gomagoma313@gmail.com>
2023-07-24 13:51:21 +0900

    Google Documentに追記するように変更

--- google_apps_script/src/createDocument.ts
+++ google_apps_script/src/createDocument.ts
@@ -1,18 +1,17 @@
 /**
  * Googleドキュメント作成。リンクを知っている全員に閲覧権限
  */
-function createDocument(summary: Summary, original: string) {
+function createDocument(summary: Summary) {
   const doc = DocumentApp.create(summary.title)
   const docBody = doc.getBody()
-  docBody.setText(summary.body).appendPageBreak()
-  docBody.appendParagraph(original)
+  docBody.setText(summary.body)
 
   const fileId = doc.getId()
   const docFile = DriveApp.getFileById(fileId)
   moveDocument(docFile)
   docFile.setSharing(FILE_PERMISSION, FILE_PERMISSION_TYPE);
 
-  return fileId
+  return doc
 }
 
 function moveDocument(file: GoogleAppsScript.Drive.File) {
@@ -31,6 +30,13 @@ function createNestedFolder(path: string): GoogleAppsScript.Drive.Folder {
 
   return parent;
 }
+
+function appendToDocument(content: string, doc: GoogleAppsScript.Document.Document) {
+  const docBody = doc.getBody()
+  docBody.appendPageBreak()
+  docBody.appendParagraph(content)
+}
+
 
 function getOrCreateFolder(parent: GoogleAppsScript.Drive.Folder, folderName: string) {
   const folders = parent.getFoldersByName(folderName);
